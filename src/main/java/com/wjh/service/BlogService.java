2e49dca307acd07f4f2e9fde0fae61ac3e5765e9
junhanwang@umass.edu <junhanwang@umass.edu>
2020-12-11 02:35:02 -0500

    update

---
+++ src/main/java/com/wjh/service/BlogService.java
@@ -0,0 +1,36 @@
+package com.wjh.service;
+
+import com.wjh.po.Blog;
+import com.wjh.vo.BlogQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BlogService {
+
+    Blog getBlog(Long id);
+
+    Blog getAndConvert(Long id);
+
+    Page<Blog> listBlog(Pageable pageable, BlogQuery blog);
+
+    Page<Blog> listBlog(Pageable pageable);
+
+    Page<Blog> listBlog(Pageable pageable, Long tagId);
+
+    Page<Blog> listBlog(String query, Pageable pageable);
+
+    List<Blog> listRecommendBlogTop(Integer size);
+
+    Map<String, List<Blog>> archiveBlog();
+
+    Long countBlog();
+
+    Blog saveBlog(Blog blog);
+
+    Blog updateBlog(Long id, Blog blog);
+
+    void deleteBlog(Long id);
+}
---
+++ src/main/java/com/wjh/service/UserService.java
@@ -0,0 +1,9 @@
+package com.wjh.service;
+
+import com.wjh.po.User;
+
+public interface UserService {
+
+    User checkUser(String username, String password);
+
+}
