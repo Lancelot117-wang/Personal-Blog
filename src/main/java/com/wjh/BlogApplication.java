08485d434169a280555e483eb309cd358501636f
Junhan <junhanwang@umass.edu>
2021-12-01 21:24:53 -0600

    enable datastore repository

--- src/main/java/com/wjh/BlogApplication.java
+++ src/main/java/com/wjh/BlogApplication.java
@@ -2,8 +2,10 @@ package com.wjh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.data.datastore.repository.config.EnableDatastoreRepositories;
 
 @SpringBootApplication
+@EnableDatastoreRepositories
 public class BlogApplication {
 
     public static void main(String[] args) {
